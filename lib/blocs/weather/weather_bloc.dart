c841c02fcfc50a3f7b310decee6808def65cc389
Windy Sabtami <windysabtami92@gmail.com>
2023-06-22 17:22:44 +0800

    perubahan

--- lib/blocs/weather/weather_bloc.dart
+++ lib/blocs/weather/weather_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:weather_bloc/data/repositories/weather_repository.dart';
 
 import '../../data/models/custom_error.dart';
 import '../../data/models/weather.dart';
@@ -8,9 +9,22 @@ part 'weather_event.dart';
 part 'weather_state.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc() : super(WeatherState.initial()) {
-    on<WeatherEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final WeatherRepository weatherRepository;
+  WeatherBloc({required this.weatherRepository})
+      : super(WeatherState.initial()) {
+    on<FetchWeatherEvent>(_fetchWeather);
+  }
+
+  Future<void> _fetchWeather(
+    FetchWeatherEvent event,
+    Emitter<WeatherState> emit,
+  ) async {
+    emit(state.copyWith(status: WeatherStatus.loading));
+    try {
+      final Weather weather = await weatherRepository.fetcWeather(event.city);
+      emit(state.copyWith(status: WeatherStatus.loaded, weather: weather));
+    } on CustomError catch (e) {
+      emit(state.copyWith(status: WeatherStatus.error, error: e));
+    }
   }
 }
---
+++ lib/ui/widgets/error_dialog.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void errorDialog(BuildContext context, String errorMessage) {
+  if (!Platform.isIOS) {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => CupertinoAlertDialog(
+        title: const Text('Error'),
+        content: Text(errorMessage),
+        actions: [
+          CupertinoDialogAction(
+              child: const Text('OK'), onPressed: () => Navigator.pop(context))
+        ],
+      ),
+    );
+  } else {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+
+      builder: (context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(errorMessage),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('OK'),
+          ),
+        ]
+      )
+    );
+  }
+}
